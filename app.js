0213fb1751dcab18b894eece7deccd33f41e8c96
rashanjyot <rashanjyot@yahoo.com>
2017-09-24 14:12:21 +0530

    express-favicon

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ app.use(bodyParser.json());
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
-
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'public')));
 // Connect to the database before starting the application server.
 mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
     if (err) {
