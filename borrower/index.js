cbad1f3af8effe0814c4062b638c080ca43ea162
rashanjyot <rashanjyot@yahoo.com>
2017-10-02 02:38:55 +0530

    borrower routing

--- borrower/index.js
+++ borrower/index.js
@@ -4,15 +4,62 @@ var bodyparser=require('body-parser');
 var urlencodedParser=bodyparser.urlencoded({extended:false});
 var mongoose=require('mongoose');
 var bSchema=require('../schemas').prototype.bSchema;
+var jwt=require('jsonwebtoken');
+var cookieParser = require('cookie-parser');
+
+router.use(cookieParser());
+
+//for authentication purpose
+router.all('*',function (req, res,next) {
+  //  console.log("Intercepted");
+    //console.log(req.cookies.xtoken);
+
+    if(req.cookies.xtoken!=undefined)
+    {
+        jwt.verify(req.cookies.xtoken, 'secret', function(err, decoded) {
+
+            if(err)
+            {
+                res.locals.tokenFlag=false;
+                next();
+            }
+
+
+            if(decoded!=undefined){
+                res.locals.tokenFlag=true;
+                res.locals.xtoken=decoded;
+            }
+
+            else
+            {
+                res.locals.tokenFlag=false;
+                next();
+            }
+
+        });
+
+    }
+    else
+        res.locals.tokenFlag=false;
+
+    console.log(res.locals.tokenFlag);
+    next();
+})
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     res.render('index', { title: 'Express' });
 });
 
 
 
-router.get('/signIn',function (req,res) {
+router.get('/signIn',function (req,res,next) {
+    if(res.locals.tokenFlag)
+    {
+        res.redirect('./home');
+        next();
+    }
+
     console.log(__dirname);
     res.sendFile(__dirname+'/views/bSignIn.html')
 
@@ -29,18 +76,26 @@ router.post('/signIn',urlencodedParser,function (req,res) {
             res.sendFile(__dirname+'/views/bSignIn.html');
         else
         {
-            console.log('%s %s is a %s.', person.name, person.email, person.password) // Space Ghost is a talk show host.
+
+            //console.log('%s %s is a %s.', person.name, person.email, person.password) // Space Ghost is a talk show host.
             if(person.password===req.body.password)
             {
                 var obj={
                     name:person.name,
                     email:person.email
                 }
-                // var token = jwt.sign(obj, 'secret', {
+                var token = jwt.sign(obj, 'secret', {
+
+                });
+                // res.writeHead(200, {
+                //     'Set-Cookie': 'token='+token,
                 //
                 // });
-                // res.render('bHome',{token:token});
-                res.render('./bHome',{token:1});
+                res.cookie('xtoken',token);
+
+                res.redirect('./home')
+
+
 
 
             }
@@ -53,5 +108,17 @@ router.post('/signIn',urlencodedParser,function (req,res) {
     })
 
 })
+
+
+router.get('/home',function (req,res) {
+    res.render(__dirname+'/views/bHome');
+})
+
+
+router.post('/crRequest',urlencodedParser,function (req,res) {
+
+
+})
+
 
 module.exports = router;
